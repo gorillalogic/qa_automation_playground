0cb7d918b6108c2cbc1c171f5c96add28dca1ae4
ovalerio280788 <osvamo280788@gmail.com>
2021-03-18 16:09:46 -0600

    QA Automation Playground Start

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 docker-compose down -v --rmi all && docker-compose up -d
-sleep 10
-echo "Restoring the database..."
+echo "Restoring the database in 15 secs..."
+sleep 15
 cat backup.sql | docker exec -i $(docker ps | grep -i automation_playground_db | awk '{print $1}') /usr/bin/mysql -u root --password=wordpress wordpress
