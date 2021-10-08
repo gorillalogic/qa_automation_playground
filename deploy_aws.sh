53f8b6e29a2f199ac8a2170c62014890ff64ca96
ovalerio-mersive <ovalerio@mersive.com>
2021-10-08 16:59:57 -0600

    Change AWS EC2 ip

--- deploy_aws.sh
+++ deploy_aws.sh
@@ -2,5 +2,5 @@ echo "This script should run as root user: sudo su, otherwise there could be a p
 docker-compose -f docker-compose-aws.yml down -v --rmi all && docker-compose -f docker-compose-aws.yml up -d
 echo "Restoring the database in 20 secs..."
 sleep 20
-sed -i "s/0.0.0.0:8085/52.14.147.231/g" backup.sql
+sed -i "s/0.0.0.0:8085/3.14.176.40/g" backup.sql
 cat backup.sql | docker exec -i $(docker ps | grep -i automation_playground_db | awk '{print $1}') /usr/bin/mysql -u root --password=wordpress wordpress
